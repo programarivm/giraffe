110715ecdd0c309964aee3989365d89f0b76b961
programarivm <info@programarivm.com>
2020-05-03 17:18:29 +0100

    next.config.js cleanup

--- next.config.js
+++ next.config.js
@@ -2,21 +2,9 @@ const withImages = require('next-images');
 const withCSS = require('@zeit/next-css');
 const path = require('path');
 
-/* const withImagesAndCss = () => {
-   withImages(withCSS())
-}
-
-module.exports = {
-  withImagesAndCss,
-  webpack(config) {
-    config.resolve.modules.push('./')
-    return config;
-  }
-} */
-
 module.exports = withImages(withCSS({
-    webpack(config) {
-        config.resolve.modules.push(path.resolve('./resources/js'))
-        return config
-    }
+  webpack(config) {
+    config.resolve.modules.push(path.resolve('./resources/js'))
+    return config
+  }
 }))
