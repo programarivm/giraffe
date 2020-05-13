62d2da85040cd1a0e33ab595e90553b864023ed1
programarivm <info@programarivm.com>
2020-05-13 20:11:37 +0100

    casl abilities are updated when mounting the app

--- resources/js/Giraffe.js
+++ resources/js/Giraffe.js
@@ -1,8 +1,14 @@
+import ability from '@/ability';
+import abilityRules from '@/../../storage/ability-rules.json';
 import PrivateApp from '@/components/private/App';
 import React, { Component } from 'react';
 import Session from '@/Session';
 
 export default class Giraffe extends Component {
+  componentDidMount() {
+    ability.update(abilityRules[Session.get().role]);
+  }
+
   render() {
     return (
       <div className="Giraffe">
