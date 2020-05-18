35da012d3740289cc66a4cc9a9f56096d344daa4
programarivm <info@programarivm.com>
2020-05-18 21:00:10 +0100

    html improvements for SEO purposes

--- resources/js/pages/index.js
+++ resources/js/pages/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import CookieConsent from 'react-cookie-consent';
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Footer from '@/components/public/Footer';
+import Head from 'next/head';
 import Home  from '@/components/public/Home';
 import MainNav  from '@/components/public/MainNav';
 
@@ -10,9 +11,20 @@ class IndexPage extends React.Component {
     return (
       <div>
         <CssBaseline />
-        <MainNav />
-        <Home />
-        <Footer />
+        <Head>
+          <title>Giraffe</title>
+          <meta property="og:title" content="Giraffe" key="title" />
+          <meta name="description" content="This is a React app with Redux and Next.js interacting with a Laravel API in a LEMP stack, a real-world full-stack example SPA." />
+        </Head>
+        <nav>
+          <MainNav />
+        </nav>
+        <main>
+          <Home />
+        </main>
+        <footer>
+          <Footer />
+        </footer>
         <CookieConsent>
           Cookies? Mmm, they are delicious! However this website does not collect personal data through third-party cookies.
         </CookieConsent>
