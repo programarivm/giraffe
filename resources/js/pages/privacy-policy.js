ef782f776e78c10b1cff616331ae3c4aec0fd206
programarivm <info@programarivm.com>
2020-05-19 18:56:33 +0100

    added semantic html markup to public pages

--- resources/js/pages/privacy-policy.js
+++ resources/js/pages/privacy-policy.js
@@ -2,6 +2,7 @@ import React from 'react';
 import CookieConsent from 'react-cookie-consent';
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Footer from '@/components/public/Footer';
+import Head from 'next/head';
 import MainNav  from '@/components/public/MainNav';
 import PrivacyPolicy  from '@/components/public/PrivacyPolicy';
 
@@ -10,9 +11,20 @@ class PrivacyPolicyPage extends React.Component {
     return (
       <div>
         <CssBaseline />
-        <MainNav />
-        <PrivacyPolicy />
-        <Footer />
+        <Head>
+          <title>Giraffe — Privacy Policy</title>
+          <meta property="og:title" content="Giraffe — Privacy Policy" key="title" />
+          <meta name="description" content="Quisque id diam vel quam. Pulvinar sapien et ligula ullamcorper. Dolor sed viverra ipsum nunc." />
+        </Head>
+        <nav>
+          <MainNav />
+        </nav>
+        <main>
+          <PrivacyPolicy />
+        </main>
+        <footer>
+          <Footer />
+        </footer>
         <CookieConsent>
           Cookies? Mmm, they are delicious! However this website does not collect personal data through third-party cookies.
         </CookieConsent>
--- resources/js/pages/terms-and-conditions.js
+++ resources/js/pages/terms-and-conditions.js
@@ -2,6 +2,7 @@ import React from 'react';
 import CookieConsent from 'react-cookie-consent';
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Footer from '@/components/public/Footer';
+import Head from 'next/head';
 import MainNav  from '@/components/public/MainNav';
 import TermsAndConditions  from '@/components/public/TermsAndConditions';
 
@@ -10,9 +11,20 @@ class TermsAndConditionsPage extends React.Component {
     return (
       <div>
         <CssBaseline />
-        <MainNav />
-        <TermsAndConditions />
-        <Footer />
+        <Head>
+          <title>Giraffe — Terms and Conditions</title>
+          <meta property="og:title" content="Giraffe — Terms and Conditions" key="title" />
+          <meta name="description" content="Enim diam vulputate ut pharetra sit amet aliquam id diam. Ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at augue eget." />
+        </Head>
+        <nav>
+          <MainNav />
+        </nav>
+        <main>
+          <TermsAndConditions />
+        </main>
+        <footer>
+          <Footer />
+        </footer>
         <CookieConsent>
           Cookies? Mmm, they are delicious! However this website does not collect personal data through third-party cookies.
         </CookieConsent>
